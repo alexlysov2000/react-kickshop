389464cb4f755a1f11d758cd5c4d3f4146a1ab4d
Александр Лысов <test@gmail.com>
2025-09-13 13:08:12 +0300

    создал компоненты хедер, дравер, кард

--- src/App.js
+++ src/App.js
@@ -1,81 +1,12 @@
+import Card from './components/Card';
+import Header from './components/Header';
+import Drawer from './components/Drawer';
+
 function App() {
   return (
     <div className="wrapper clear">
-      <div className="overlay">
-        <div className="drawer">
-          <h2 className="mb-30 d-flex justify-between">Корзина<img className="removeBtn cu-p" src="/img/btn-remove.svg" alt="remove" /></h2>
-
-          <div className="items">
-            <div className="cartItem d-flex align-center mb-20">
-              {/* <img className="mr-20" width={70} height={70} src="/img/sneakers/1.png" alt="sneakers" /> */}
-
-              <div style={{ backgroundImage: 'url(/img/sneakers/1.png)' }} className="cartItemImg"></div>
-
-              <div className="mr-20 flex">
-                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-                <b>12 999 руб.</b>
-              </div>
-              <img className="removeBtn" src="/img/btn-remove.svg" alt="remove" />
-            </div>
-
-            <div className="cartItem d-flex align-center mb-20">
-              {/* <img className="mr-20" width={70} height={70} src="/img/sneakers/1.png" alt="sneakers" /> */}
-
-              <div style={{ backgroundImage: 'url(/img/sneakers/1.png)' }} className="cartItemImg"></div>
-
-              <div className="mr-20 flex">
-                <p className="mb-5">Мужские Кроссовки Nike Air Max 270</p>
-                <b>12 999 руб.</b>
-              </div>
-              <img className="removeBtn" src="/img/btn-remove.svg" alt="remove" />
-            </div>
-          </div>
-
-          <div className="cartTotalBlock">
-            <ul>
-              <li>
-                <span>Итого:</span>
-                <div></div>
-                <b>21 498 руб.</b>
-              </li>
-              <li>
-                <span>Налог 5%:</span>
-                <div></div>
-                <b>1074 руб. </b>
-              </li>
-            </ul>
-
-            <button className="greenButton" >Оформить заказ <img src="/img/arrow.svg" alt="arrow" /></button>
-          </div>
-
-        </div>
-      </div>
-
-      <header className="d-flex justify-between align-center p-40">
-        <div className="d-flex align-center">
-          {/* не надо указывать папку из которой доставать */}
-          <img width={40} height={40} src="/img/logo.png" />
-          <div>
-            <h3 className="text-uppercase">Sneaker store react</h3>
-            <p className="opacity-5">Магазин лучших кроссовок</p>
-          </div>
-        </div>
-
-        <ul className="d-flex">
-          <li className="mr-30">
-            <img width={18} height={18} src="/img/cart.svg" />
-            <span>1205 руб.</span>
-          </li>
-
-          <li className="mr-30">
-            <img width={18} height={18} src="/img/heart-light.svg" alt="heart-light" />
-          </li>
-
-          <li>
-            <img width={18} height={18} src="/img/user.svg" />
-          </li>
-        </ul>
-      </header>
+      <Drawer />
+      <Header />
 
       <div className="content p-40">
 
@@ -88,64 +19,8 @@ function App() {
         </div>
 
         <div className="d-flex">
-          <div className="card">
-            <div className="favorite">
-              <img src="/img/heart-light.svg" alt="heart-light" />
-            </div>
-            <img width={133} height={112} src="/img/sneakers/1.png" alt="sneakers" />
-            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column">
-                <span>Цена:</span>
-                <b>12 999 руб.</b>
-              </div>
-              <button className="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-              </button>
-            </div>
-          </div>
+          <Card />
 
-          <div className="card">
-            <img width={133} height={112} src="/img/sneakers/2.png" alt="sneakers" />
-            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column">
-                <span>Цена:</span>
-                <b>12 999 руб.</b>
-              </div>
-              <button className="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-              </button>
-            </div>
-          </div>
-
-          <div className="card">
-            <img width={133} height={112} src="/img/sneakers/3.png" alt="sneakers" />
-            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column">
-                <span>Цена:</span>
-                <b>12 999 руб.</b>
-              </div>
-              <button className="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-              </button>
-            </div>
-          </div>
-
-          <div className="card">
-            <img width={133} height={112} src="/img/sneakers/4.png" alt="sneakers" />
-            <h5>Мужские Кроссовки Nike Blazer Mid Suede</h5>
-            <div className="d-flex justify-between align-center">
-              <div className="d-flex flex-column">
-                <span>Цена:</span>
-                <b>12 999 руб.</b>
-              </div>
-              <button className="button">
-                <img width={11} height={11} src="/img/plus.svg" alt="Plus" />
-              </button>
-            </div>
-          </div>
         </div>
       </div>
     </div>
---
+++ src/components/Header.js
@@ -0,0 +1,31 @@
+function Header() {
+    return (
+        <header className="d-flex justify-between align-center p-40">
+            <div className="d-flex align-center">
+                {/* не надо указывать папку из которой доставать */}
+                <img width={40} height={40} src="/img/logo.png" />
+                <div>
+                    <h3 className="text-uppercase">Sneaker store react</h3>
+                    <p className="opacity-5">Магазин лучших кроссовок</p>
+                </div>
+            </div>
+
+            <ul className="d-flex">
+                <li className="mr-30">
+                    <img width={18} height={18} src="/img/cart.svg" />
+                    <span>1205 руб.</span>
+                </li>
+
+                <li className="mr-30">
+                    <img width={18} height={18} src="/img/heart-light.svg" alt="heart-light" />
+                </li>
+
+                <li>
+                    <img width={18} height={18} src="/img/user.svg" />
+                </li>
+            </ul>
+        </header>
+    );
+}
+
+export default Header; 
