9fc702780adc66eef13ba37d418cc321bf1d92d7
Александр Лысов <test@gmail.com>
2025-09-16 08:38:34 +0300

    урок 3

--- src/App.js
+++ src/App.js
@@ -2,6 +2,16 @@ import Card from './components/Card';
 import Header from './components/Header';
 import Drawer from './components/Drawer';
 
+//входные данные массива
+const arr = [
+  { name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 12999, imageUrl: '/img/sneakers/1.png' },
+  { name: 'Мужские Кроссовки Nike Air Max 270', price: 15600, imageUrl: '/img/sneakers/2.png' },
+  { name: 'Мужские Кроссовки Nike Blazer Mid Suede', price: 8499, imageUrl: '/img/sneakers/3.png' },
+  { name: 'Кроссовки Puma X Aka Boku Future Rider', price: 8999, imageUrl: '/img/sneakers/4.png' },
+];
+
+
+
 function App() {
   return (
     <div className="wrapper clear">
@@ -19,7 +29,17 @@ function App() {
         </div>
 
         <div className="d-flex">
-          <Card />
+
+
+          {/* функция вывода карточки из массива */}
+          {arr.map((obj) => (
+            <Card
+              title={obj.name}
+              price={obj.price}
+              imageUrl={obj.imageUrl}
+              onClick={() => console.log(obj)}
+            />
+          ))}
 
         </div>
       </div>
